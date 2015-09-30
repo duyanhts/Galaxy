3ea4929fe05881b6e78e9f52bd134660b56ffa8f
duyanhts <duyanh0205@gmail.com>
2015-09-30 13:13:10 +0700

    Signed-off-by: duyanhts <duyanh0205@gmail.com>

---
+++ platforms/wp8/www/cordova_plugins.js
@@ -0,0 +1,18 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.devgeeks.Canvas2ImagePlugin/www/Canvas2ImagePlugin.js",
+        "id": "org.devgeeks.Canvas2ImagePlugin.Canvas2ImagePlugin",
+        "clobbers": [
+            "window.canvas2ImagePlugin"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.1.0",
+    "org.devgeeks.Canvas2ImagePlugin": "0.6.0"
+}
+// BOTTOM OF METADATA
+});
